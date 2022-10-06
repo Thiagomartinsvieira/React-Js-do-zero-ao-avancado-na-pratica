3c6f1f47eee149d33093a1deca38e920d5b455d6
Thiagomartinsvieira <thiagoalagadico@gmail.com>
2022-10-06 18:13:23 -0300

    aula 09 concluída

--- aula09/script.js
+++ aula09/script.js
@@ -1,2 +1,11 @@
-var nome = localStorage.nome;
-console.log(nome);
+
+
+var nome = '';
+
+if (typeof localStorage.nome == 'undefined'){
+    localStorage.nome = prompt("Digite seu nome");
+}
+
+nome = localStorage.nome;
+
+document.getElementById('nome').innerHTML = nome;
